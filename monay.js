fd426d71bc24333e0ee573d31cf26780634b63f6
sintendo <espeon.78s@gmail.com>
2019-06-21 20:23:34 -0400

    Update monay.js

--- monay.js
+++ monay.js
@@ -1,8 +1,12 @@
 module.exports = {
 run: function(message) {
 	if (message.content == "joker") {
-		message.react("🃏");
+		message.react('🃏');
 	}
+	
+	if (message.content == "react") {
+		message.react('🔴').then(() => message.react('🔵'));
+	}	
 	
 	if (message.content == "ping") {
 		message.channel.send("pong");
